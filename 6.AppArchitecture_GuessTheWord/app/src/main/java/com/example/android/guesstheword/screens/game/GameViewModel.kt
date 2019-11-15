87f5a63829bd35ddd306788107d43e4fa5a453c3
Ankur Aggarwal <roc.windwarrior@gmail.com>
2019-11-15 11:46:02 +0530

    feat : Add LiveData
    - Add LiveData for Word and Score, this allows data retention during
    screen rotation
    - Add LiveData for game finished. This facilitates navigation at end
    - Add encapsulation of LiveData

--- 6.AppArchitecture_GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ 6.AppArchitecture_GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,20 +1,33 @@
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = ""
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
 
     // The current score
-    var score = 0
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    private val _eventGameFinished = MutableLiveData<Boolean>()
+    val eventGameFinished : LiveData<Boolean>
+        get() = _eventGameFinished
+
     init {
         Timber.i("init block called")
+        _score.value = 0
+        _word.value = ""
+        _eventGameFinished.value = false
         resetList()
         nextWord()
     }
@@ -27,12 +40,12 @@ class GameViewModel : ViewModel() {
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        _score.value = (_score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        _score.value = (_score.value)?.plus(1)
         nextWord()
     }
 
@@ -72,9 +85,13 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            //gameFinished()
+            _eventGameFinished.value = true
         } else {
-            word = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
+    }
+
+    fun onGameFinishCompleted(){
+        _eventGameFinished.value = false
     }
 }
